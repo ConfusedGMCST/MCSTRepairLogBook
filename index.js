05e9df43fbd25fc77833b62c8331bed132ab7ab3
ConfusedG <drippychestercheetah02@gmail.com>
2026-01-09 09:03:11 -0500

    added requirement function
    some fields are required for the log
    
    currently working on the finish button

--- index.js
+++ index.js
@@ -14,13 +14,17 @@ function inputDiv(text, type, parent) {
 }
 
 //input property assembles both subDiv and inputDiv to create the sub container for storing a certain value in the log
-function inputProperty(text, type, parentContainer, required = false) {
+function inputProperty(text, type, parentContainer, required = true) {
     const container = document.createElement('div')
     container.className = "input-row"
+    container.dataset.required = required
     inputDiv(text, type, container)
     parentContainer.appendChild(container)
-    if (!required) {
-        
+    if (required) {
+        const star = document.createElement('h3')
+        star.textContent = "*"
+        star.className = "required"
+        container.appendChild(star)
     }
     return container //returns the container to a variable
 }
@@ -41,13 +45,30 @@ function initLog() {
     const procedureContainer = inputProperty("Procedure", 'text', container, required = false)
     const extraContainer = inputProperty("Extra Notes", 'text', container, required = false)
     
-    const finishButton = document.createElement('button')
-    finishButton.textContent = "Finish" //add functionality later
+    const containers = [titleContainer, partContainer, costContainer, personContainer, dateContainer, procedureContainer, extraContainer]
 
+    const finishButton = document.createElement('button')
+    finishButton.textContent = "Finish"
     container.appendChild(finishButton)
     document.body.appendChild(container)
+
+    finishButton.addEventListener("click", () => { //apologies for awful nesting
+        for (i = 0; i < containers.length;) {
+            const input = containers[i].querySelector("input")
+            const required = containers[i].dataset.required
+            if (required) {
+                if (input.type == 'text') {
+                    if (!(input.textContent == "")) {
+
+                    }
+                }
+            }
+        }
+    })
 }
 
 createButton.addEventListener("click", () => {
-    if (!makingLog) initLog()
+    if (!makingLog) {
+        initLog()
+    }
 })
